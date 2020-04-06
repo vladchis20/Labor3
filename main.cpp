e4774b499da6cfb941ef45fdb8ae9683822c0b85
vladchis20 <59486273+vladchis20@users.noreply.github.com>
2020-04-06 22:53:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Node.h"
+#include "Tree.h"
+#include "Teste.h"
+
+int main()
+{
+	TestInPostPreOrder();
+	TestCount();
+	TestInsert();
+	cout << "\n\nTests over!";
+}
